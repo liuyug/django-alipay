fffabc34a79b8916a9c6c6dbf52200541bb06df4
liuyug <liuyug@yahoo.cn>
2013-11-20 14:00:51 +0800

    Fix

--- alipay/create_direct_pay_by_user/dpn/urls.py
+++ alipay/create_direct_pay_by_user/dpn/urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('alipay.create_direct_pay_by_user.dpn.views',
     url(r'^$', 'dpn', {'item_check_callable':None}, name='alipay-dpn'),
--- alipay/create_partner_trade_by_buyer/ptn/urls.py
+++ alipay/create_partner_trade_by_buyer/ptn/urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('alipay.create_partner_trade_by_buyer.ptn.views',
     url(r'^$', 'ptn', {'item_check_callable':None}, name='alipay-ptn'),
