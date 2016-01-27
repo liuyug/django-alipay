2190408534ddf09bb46ef992210911828c9da052
Tang Ziya <tcztzy@gmail.Com>
2016-01-27 14:55:09 +0800

    Fix the import error
    There is an ImportError in widgets.py

--- alipay/widgets.py
+++ alipay/widgets.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from django import forms
-from django.forms.util import flatatt
+from django.forms.utils import flatatt
 from django.utils.safestring import mark_safe
 from django.utils.encoding import force_unicode
 
