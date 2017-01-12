5dfdcc72f6a4aea85689036304046b6c598e86ce
LIU Yugang <liuyug@gmail.com>
2017-01-12 12:53:01 +0800

    add appconfig

---
+++ alipay/create_direct_pay_by_user/dpn/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class AlipayDPNConfig(AppConfig):
+    name = 'alipay.create_direct_pay_by_user.dpn'
+    label = 'alipay_direct_pay'
---
+++ alipay/create_partner_trade_by_buyer/ptn/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class AlipayPTNConfig(AppConfig):
+    name = 'alipay.create_partner_trade_by_buyer.ptn'
+    label = 'alipay_partner_trade'
